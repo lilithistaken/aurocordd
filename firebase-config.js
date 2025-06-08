d35b5637bc09b67c90c7181cfdc44dcbac847c14
lilithistaken <miuraclara778@gmail.com>
2025-06-08 23:19:52 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBkU8GgOP09YYfOmdvpypPRoXYk-SUFqWI",
   authDomain: "chatting-87797.firebaseapp.com",
+  databaseURL: "https://chatting-87797-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "chatting-87797",
+  storageBucket: "chatting-87797.firebasestorage.app",
+  messagingSenderId: "767603735256",
   appId: "1:767603735256:web:14bf5c7696fa3f7e1835ad",
+  measurementId: "G-Q4JVCG9NR4"
 };
 
 // Export the config
